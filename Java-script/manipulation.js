5a64a4b2d5e7a6b448ccafdf280d12f433fab404
siddikha0167 <siddikha0167@gmail.com>
2026-06-11 23:59:18 +0530

    js DAY 5

---
+++ Java-script/manipulation.js
@@ -0,0 +1,12 @@
+//Get the wish element
+let greet=document.getElementById("wish");
+console.log(greet);
+let baloon= document.getElementsByClassName("celebration-text");
+console.log(baloon);
+let cake = document.getElementsByTagName("div");
+console.log(cake);
+let candel = document.querySelector(".celebration-text");
+console.log(candel);
+let nife = document.querySelectorAll("p");
+console.log(nife);
+
