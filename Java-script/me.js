b297d0f23549495c55d683531c04a2a793460371
siddikha0167 <siddikha0167@gmail.com>
2026-06-13 11:46:07 +0530

    licence

--- Java-script/me.js
+++ Java-script/me.js
@@ -1,28 +1,68 @@
-document.getElementById("details")
-.addEventListener("submit",function(event)
-{
-    event.preventDefault();
-    let Name = document.getElementById("name").value;
-    let Email = document.getElementById("mail").value;
-    let Phone = document.getElementById("phone").value;
-    let Age = document.getElementById("age").value;
-    let Gender = document.getElementById("gender").value;
-    let Address = document.getElementById("address").value;
-    document.getElementById("p1").innerText="Name:"+Name;
-    document.getElementById("p2").innerText="Email:"+Email;
-    document.getElementById("p3").innerText="Mobile:"+Phone;
-    document.getElementById("p4").innerText="Age:"+Age;
-    document.getElementById("p5").innerText="Gender:"+Gender;
-    document.getElementById("p6").innerText="Address:"+Address;
+// Code for page1.html
+let form = document.getElementById("details");
 
-    let image = document.getElementById("image").files[0];
-    if(image)
-    {
-        let read = new FileReader();
-        read.onload = function(e)
-        {
-            document.getElementById("dp").src=e.target.result;
-        };
-        read.readAsDataURL(image);
-    }
-});
+if (form) {
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        localStorage.setItem("name",
+            document.getElementById("name").value);
+
+        localStorage.setItem("mail",
+            document.getElementById("mail").value);
+
+        localStorage.setItem("phone",
+            document.getElementById("phone").value);
+
+        localStorage.setItem("age",
+            document.getElementById("age").value);
+
+        localStorage.setItem("gender",
+            document.getElementById("gender").value);
+
+        localStorage.setItem("address",
+            document.getElementById("address").value);
+
+        let image = document.getElementById("image").files[0];
+
+        if (image) {
+            let reader = new FileReader();
+
+            reader.onload = function(e) {
+                localStorage.setItem("photo", e.target.result);
+
+                window.location.href = "licence.html";
+            };
+
+            reader.readAsDataURL(image);
+        } else {
+            window.location.href = "licence.html";
+        }
+    });
+}
+
+
+// Code for licence.html
+if (document.getElementById("p1")) {
+
+    document.getElementById("p1").innerText =
+        "Name: " + localStorage.getItem("name");
+
+    document.getElementById("p2").innerText =
+        "Email: " + localStorage.getItem("mail");
+
+    document.getElementById("p3").innerText =
+        "Phone: " + localStorage.getItem("phone");
+
+    document.getElementById("p4").innerText =
+        "Age: " + localStorage.getItem("age");
+
+    document.getElementById("p5").innerText =
+        "Gender: " + localStorage.getItem("gender");
+
+    document.getElementById("p6").innerText =
+        "Address: " + localStorage.getItem("address");
+
+    document.getElementById("dp").src =
+        localStorage.getItem("photo");
+}
